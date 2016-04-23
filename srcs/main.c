d3e2278d2f6654b5856c4150617b806e941461c3
roche_g <guillaume.roche@epitech.eu>
2016-04-24 01:29:29 +0200

    created srcs dir and main file

---
+++ srcs/main.c
@@ -0,0 +1,21 @@
+/*
+    This file is part of ftsh.
+
+    ftsh is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+    ftsh is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ftsh.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+int	main(int ac, char **av)
+{
+  (void)ac;
+  (void)av;
+  return (0);
+}
